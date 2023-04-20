46312b868a4d71ccd48e3a4146ac558e68c6abab
codesche <110509654+codesche@users.noreply.github.com>
2023-04-20 14:06:20 +0900

    Create Solution.java

---
+++ Apr20/Solution.java
@@ -0,0 +1,30 @@
+
+// 프로그래머스 - 직사각형 넓이 구하기
+
+public class Solution {
+    public int solution(int[][] dots) {
+        int w = 0;
+        int h = 0;
+        int x = dots[0][0];
+        int y = dots[0][1];
+        for (int i = 1; i < dots.length; i++) {
+            if (x != dots[i][0]) {
+                w = Math.abs(x - dots[i][0]);
+            }
+
+            if (y != dots[i][1]) {
+                h = Math.abs(y - dots[i][1]);
+            }
+        }
+
+        return w * h;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 1}, {2, 1}, {2, 2}, {1, 2}};
+
+        Solution T = new Solution();
+
+        System.out.println(T.solution(array));
+    }
+}
