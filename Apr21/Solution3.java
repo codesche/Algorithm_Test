4f763c555f18c00dad1cc1a922ae3ed93bddd2cc
codesche <110509654+codesche@users.noreply.github.com>
2023-04-21 19:19:28 +0900

    Create Solution3.java

---
+++ Apr21/Solution3.java
@@ -0,0 +1,13 @@
+
+
+// 프로그래머스 - 문자열 변환
+
+public class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        answer = String.valueOf(n);
+
+        return answer;
+    }
+}
