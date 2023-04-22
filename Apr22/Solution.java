5db7c0e8c11428f7ab508d57d449bbbf45b8aaf9
codesche <110509654+codesche@users.noreply.github.com>
2023-04-22 12:58:38 +0900

    Create Solution.java

---
+++ Apr22/Solution.java
@@ -0,0 +1,16 @@
+
+
+
+// 문자열 반복
+
+public class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+
+        for (int i = 0; i < k; i++) {
+            answer += my_string;
+        }
+
+        return answer;
+    }
+}
