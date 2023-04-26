4600e08c0ec2f6098de8ab41cd04d0736d79b743
codesche <110509654+codesche@users.noreply.github.com>
2023-04-26 13:41:35 +0900

    Create Solution.java

---
+++ Apr26/Solution.java
@@ -0,0 +1,25 @@
+
+// 프로그래머스 이어 붙인 수
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String evenString = "";
+        String oddString = "";
+        
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                evenString += String.valueOf(num_list[i]);
+            } else {
+                oddString += String.valueOf(num_list[i]);
+            }
+        }
+        
+        int evenInteger = Integer.parseInt(evenString);
+        int oddInteger = Integer.parseInt(oddString); 
+        
+        answer = evenInteger + oddInteger;
+        
+        return answer;
+    }
+}
