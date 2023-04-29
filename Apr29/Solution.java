b2ceee75596c267827bc1c41388b689866ef582e
codesche <110509654+codesche@users.noreply.github.com>
2023-04-29 14:03:01 +0900

    Create Solution.java

---
+++ Apr29/Solution.java
@@ -0,0 +1,23 @@
+
+// 문자열 바꿔서 찾기 - 프로그래머스
+
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        StringBuilder temp = new StringBuilder();
+
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == 'A') {
+                temp.append('B');
+            } else if (myString.charAt(i) == 'B') {
+                temp.append('A');
+            }
+        }
+
+        if (temp.toString().contains(pat)) {
+            answer = 1;
+        } 
+
+        return answer;
+    }
+}
