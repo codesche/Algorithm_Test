b7330443896b36f01dd518afec9760f0f524e5bd
codesche <110509654+codesche@users.noreply.github.com>
2023-05-03 12:35:41 +0900

    Create Solution.java

---
+++ May03/Solution.java
@@ -0,0 +1,25 @@
+
+
+// 프로그래머스 - 문자열 뒤집기
+
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        
+        char[] array = my_string.toCharArray();
+        
+        for (int i = 0; i < s; i++) {
+            answer += array[i];
+        }
+        
+        for (int i = e; i >= s; i--) {
+            answer += array[i];
+        }
+        
+        for (int i = e + 1; i < array.length; i++) {
+            answer += array[i];
+        }
+        
+        return answer;
+    }
+}
